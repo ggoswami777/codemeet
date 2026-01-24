1318a5ef66a9585eefaefd1688f77dd3bc795afd
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-01-24 12:15:27 +0530

    db and tools setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,35 @@
-import express from "express"
-import path from "path"
+import express from "express";
+import path from "path";
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 
-const app=express();
-const __dirname=path.resolve();
-app.get('/health',(req,res)=>{
-    res.status(200).json({msg:"api is up and running"})
-})
-app.get('/books',(req,res)=>{
-    res.status(200).json({msg:"this is the books endpoint"})
-})
+const app = express();
+const __dirname = path.resolve();
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
+});
+app.get("/books", (req, res) => {
+  res.status(200).json({ msg: "this is the books endpoint" });
+});
 
-if(ENV.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
-    
-    app.get("/{*any}",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
-    })
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
-app.listen(ENV.PORT,()=>console.log("Server is running on port ",ENV.PORT))
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => {
+      console.log("Server is running on port ", ENV.PORT);
+      
+    });
+  } catch (error) {
+    console.error("Error starting the server")
+  }
+};
+
+startServer();
