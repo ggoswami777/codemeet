b8f807b002c8522adbc91f5b403c0dc20303e46d
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-01-31 21:34:24 +0530

    daisy ui setup

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,19 @@
+import { SparklesIcon } from 'lucide-react'
 import React from 'react'
-
+import {Link} from 'react-router'
 function HomePage() {
   return (
-    <div>
-        Homepage
-      <button className='btn btn-ghost'>click me</button>
+    <div className='b-gradient-to-br from base-100 via-base-200 to-base-300'>
+        {/* nav */}
+        <nav className='bg-base-100/80 backdrop-blur-md border-b border-primary/20 sticky top-0 z-50 shadow-lg'>
+          <div className='max-w-7xl mx-auto p-4 flex items-center justify-center'>
+            <Link to={"/"} className='flex items-center gap-3 hover:scale-105 transition-transform duration-200'>
+              <div className='size-10 rounded-xl bg-gradient-to-br from-primary via-secondary to-accent flex items-center justify-center shadow-lg'>
+                <SparklesIcon/>
+              </div>
+            </Link>
+          </div>
+        </nav>
     </div>
   )
 }
